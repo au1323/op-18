2af4de60eecd3e5624ff10f82105c06a2b43c662
au1323 <50653816+au1323@users.noreply.github.com>
2021-08-29 19:35:57 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,5 +17,6 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-git clone https://github.com/kenzok8/small-package package/openwrt-package
+#git clone https://github.com/kenzok8/small-package package/openwrt-package
+git clone https://github.com/liuran001/openwrt-packages package/openwrt-package
 git clone https://github.com/kenzok8/small package/openwrt-small
